5afa6ff21b1e266498c10c0851b38ea71f85bab6
Josh <relic1337@gmail.com>
2026-01-17 14:57:15 -0500

    Module 2 Completion: Colored Projectiles

--- Source/firstperson415/firstperson415PickUpComponent.cpp
+++ Source/firstperson415/firstperson415PickUpComponent.cpp
@@ -8,14 +8,6 @@ Ufirstperson415PickUpComponent::Ufirstperson415PickUpComponent()
 	SphereRadius = 32.f;
 }
 
-void Ufirstperson415PickUpComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// Register our Overlap Event
-	OnComponentBeginOverlap.AddDynamic(this, &Ufirstperson415PickUpComponent::OnSphereBeginOverlap);
-}
-
 void Ufirstperson415PickUpComponent::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	// Checking if it is a First Person Character overlapping
